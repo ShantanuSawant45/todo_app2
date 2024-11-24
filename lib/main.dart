2b5e4ac3f699b98f76dc546057b6fbbb9decaced
Shantu-pixels <shanpantech264@gmail.com>
2024-11-24 19:53:41 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/provider.dart' as provider;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app2/screens/splash_screen.dart';
 import 'Budget_tracker/providers/transaction_provider.dart';
@@ -18,45 +18,52 @@ void main() async {
   await Hive.openBox('health_data');
   final prefs = await SharedPreferences.getInstance();
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => PeriodProvider(prefs),
-      child: const MyApp(),
+    ProviderScope(
+      child: provider.MultiProvider(
+        providers: [
+          provider.ChangeNotifierProvider(
+            create: (_) => PeriodProvider(prefs),
+          ),
+          provider.ChangeNotifierProvider(
+            create: (_) => TransactionProvider(),
+          ),
+        ],
+        child: const MyApp(),
+      ),
     ),
   );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => TransactionProvider(),
-      child: MaterialApp(
-        title: 'Budget Tracker',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.indigo,
-            brightness: Brightness.light,
-          ),
-          useMaterial3: true,
+    return MaterialApp(
+      title: 'Budget Tracker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          brightness: Brightness.light,
         ),
-        routes: {
-          '/splash': (context) => const SplashScreen(),
-          '/todo-list': (context) => const  TodoScreen(),
-          '/period-tracker': (context) => const HomePage_PeriodTracker(),
-          '/health-tracker': (context) => const HomePage_healthTracker(),
-          '/budget-tracker': (context) => const Budget_tracker_screen(),
-        },
-        darkTheme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.indigo,
-            brightness: Brightness.dark,
-          ),
-          useMaterial3: true,
-        ),
-        home: const SplashScreen(),
+        useMaterial3: true,
       ),
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+        '/todo-list': (context) => const TodoScreen(),
+        '/period-tracker': (context) => const HomePage_PeriodTracker(),
+        '/health-tracker': (context) => const HomePage_healthTracker(),
+        '/budget-tracker': (context) => const Budget_tracker_screen(),
+      },
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
     );
   }
 }
